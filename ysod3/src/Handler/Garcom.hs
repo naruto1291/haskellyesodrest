f5327936a0fc7d04ade6a94c1d51db913a8d9afb
Ramon Fernandes <ramon.fer.lou@outlook.com>
2018-11-14 11:58:26 -0200

    Add files via upload
    atualizar tabela modelo e adicionar rota post garcomaddR

--- ysod3/src/Handler/Garcom.hs
+++ ysod3/src/Handler/Garcom.hs
@@ -13,3 +13,10 @@ getGarcomEspecificaR pid senha = do
     addHeader "Access-Control-Allow-Origin" "*"
     prod <- runDB $ selectList [GarcomNome ==. pid,GarcomSenha ==. senha] []
     sendStatusJSON ok200 (toJSON  prod)
+    
+postGarcomaddR :: Handler TypedContent
+postGarcomaddR = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    produto <- requireJsonBody :: Handler Garcom
+    pid <- runDB $ insert produto
+    sendStatusJSON created201 (toJSON pid)   
