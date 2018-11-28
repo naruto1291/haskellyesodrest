cc7d692d3953aad80a528e0e727469d434c9b0b3
FelipeArakaki <41403275+FelipeArakaki@users.noreply.github.com>
2018-11-28 20:47:40 -0200

    Add files via upload
    método post e rota de compra

--- ysod3/src/Handler/Compra.hs
+++ ysod3/src/Handler/Compra.hs
@@ -15,3 +15,12 @@ getCompraR = do
    anyOriginIn
    prods <- runDB $ selectList [] [Asc CompraDataFecha] 
    sendStatusJSON ok200 (toJSON prods)
+
+postCompraAddR :: Handler TypedContent
+postCompraAddR = do
+    anyOriginIn
+    produto <- requireJsonBody :: Handler Compra
+    utct <- liftIO getCurrentTime
+    pid <- runDB $ insert $ produto {compraDataAbertura = utctDay utct, compraHoraAbertura = utct,compraHoraFecha = utct}
+    prod <- runDB $ selectList [CompraId ==. pid ] [] 
+    sendStatusJSON created201 (toJSON prod )
