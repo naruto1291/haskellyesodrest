c31c2ebe5dd971016e4dc1df60213b227961766c
Ramon Fernandes <ramon.fer.lou@outlook.com>
2018-12-01 06:26:33 -0200

    Add files via upload
    garcom login rota e handler

--- ysod3/src/Handler/Home.hs
+++ ysod3/src/Handler/Home.hs
@@ -20,3 +20,15 @@ postHomeR = do
             if ((mesaSenha user) == (mesaSenha userJ)) then
                 sendStatusJSON ok200 $ toJSON $ tokenize  uid else
                 notAuthenticated
+                
+postHomeDoisR :: Handler Value
+postHomeDoisR = do
+    anyOriginIn
+    userJ <- requireJsonBody :: Handler Garcom
+    userM <- runDB . getBy . UniqueGarcom $ garcomNome userJ
+    case userM of
+        Nothing -> notAuthenticated
+        Just (Entity uid user) ->
+            if ((garcomSenha user) == (garcomSenha userJ)) then
+                sendStatusJSON ok200 $ toJSON $ tokenize2  uid else
+                notAuthenticated 
