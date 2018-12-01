26ca844459edcac248b2304212a9e5afd47ada66
Ramon Fernandes <ramon.fer.lou@outlook.com>
2018-12-01 06:17:01 -0200

    Add files via upload
    / HomeR POST  rota e handler

--- ysod3/src/Handler/Home.hs
+++ ysod3/src/Handler/Home.hs
@@ -4,10 +4,19 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Home where
-
+import Handler.Funcs (anyOriginIn)
 import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
-getHomeR :: Handler Html
-getHomeR = undefined
+postHomeR :: Handler Value
+postHomeR = do
+    anyOriginIn
+    userJ <- requireJsonBody :: Handler Mesa
+    userM <- runDB . getBy . UniqueMesa $ mesaLogin userJ
+    case userM of
+        Nothing -> notAuthenticated
+        Just (Entity uid user) ->
+            if ((mesaSenha user) == (mesaSenha userJ)) then
+                sendStatusJSON ok200 $ toJSON $ tokenize  uid else
+                notAuthenticated
