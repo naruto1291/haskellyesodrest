9a98d823c63381a5fee9aac12ef57fbd8f645ccb
FelipeArakaki <41403275+FelipeArakaki@users.noreply.github.com>
2018-12-01 14:41:25 -0200

    Update Application.hs
    importado Pedido

--- ysod3/src/Application.hs
+++ ysod3/src/Application.hs
@@ -45,6 +45,7 @@ import Handler.Ingrediente
 import Handler.Mesa
 import Handler.Compra
 import Handler.Imagem
+import Handler.Pedido
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
