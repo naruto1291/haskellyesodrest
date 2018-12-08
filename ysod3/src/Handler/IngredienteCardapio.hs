e2fe3537946a92ba32223a32771a4f4719f54ceb
CoelhoGabrielGit <44949020+CoelhoGabrielGit@users.noreply.github.com>
2018-12-08 10:51:11 -0200

    Update IngredienteCardapio.hs
    Adicionados com seus options os métodos get e delete

--- ysod3/src/Handler/IngredienteCardapio.hs
+++ ysod3/src/Handler/IngredienteCardapio.hs
@@ -30,3 +30,28 @@ optionsIngreCardapioR :: CardapioId -> Handler TypedContent
 optionsIngreCardapioR _ = do
    anyOriginIn
    sendStatusJSON ok200 (object [])
+
+getIngreCardapiosR :: Handler TypedContent
+getIngreCardapiosR = do 
+   anyOriginIn
+   prods <- runDB $ selectList [] [Asc IngredienteCardapioId] 
+   sendStatusJSON ok200 (toJSON prods)
+
+optionsIngreCardapiosR :: Handler TypedContent
+optionsIngreCardapiosR  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+
+deleteIngreCardapioDelR :: IngredienteCardapioId -> Handler TypedContent 
+deleteIngreCardapioDelR pid =do
+        anyOriginIn
+        _ <- runDB $ get404 pid :: Handler IngredienteCardapio
+        runDB $ delete pid
+        sendStatusJSON noContent204 (object [])
+
+optionsIngreCardapioDelR ::  IngredienteCardapioId -> Handler TypedContent
+optionsIngreCardapioDelR pid =do
+    anyOriginIn
+    sendStatusJSON ok200 (object [])
+    
+ 
