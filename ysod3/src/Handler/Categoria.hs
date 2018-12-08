55168fb35576084883d33bbfdf1030d4af0d060b
CoelhoGabrielGit <44949020+CoelhoGabrielGit@users.noreply.github.com>
2018-12-08 12:43:39 -0200

    Update Categoria.hs
    Adicionado o patch e os options dos metodos

--- ysod3/src/Handler/Categoria.hs
+++ ysod3/src/Handler/Categoria.hs
@@ -27,3 +27,30 @@ getCategoriaporR pid = do
     anyOriginIn
     prod <- runDB $ selectList [CategoriaId ==. pid] []
     sendStatusJSON ok200 (toJSON  prod)
+
+patchCategoriaupdateR :: CategoriaId -> Text -> Handler TypedContent
+patchCategoriaupdateR pid nome = do 
+    _ <- runDB $ get404 pid
+    anyOriginIn
+    runDB $ update pid [CategoriaNome =.  nome ] --readMaybe 
+    sendStatusJSON noContent204 (object [])
+
+optionsCategoriaupdateR :: CategoriaId -> Text -> Handler TypedContent
+optionsCategoriaupdateR pid nome = do 
+    anyOriginIn
+    sendStatusJSON ok200 (object [])
+    
+optionsCategoriaporR ::CategoriaId  -> Handler TypedContent
+optionsCategoriaporR __ = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+
+optionsCategoriaAddR :: Handler TypedContent
+optionsCategoriaAddR  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+
+optionsCategoriaR :: Handler TypedContent
+optionsCategoriaR  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
