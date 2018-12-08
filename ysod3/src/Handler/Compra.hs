03384231490cd6e7a2a3db9b340ecbef6d55b916
CoelhoGabrielGit <44949020+CoelhoGabrielGit@users.noreply.github.com>
2018-12-08 12:39:56 -0200

    Update Compra.hs
    Completado a compra com seus métodos e options

--- ysod3/src/Handler/Compra.hs
+++ ysod3/src/Handler/Compra.hs
@@ -8,19 +8,105 @@ module Handler.Compra where
 
 import Import
 
+import Import
+import Data.Text as T (pack)
+import Prelude as R (read)
 import Handler.Funcs (anyOriginIn)
 
-getCompraR :: Handler TypedContent
-getCompraR = do 
+postFechamentoAddR :: Handler TypedContent
+postFechamentoAddR = do
+    anyOriginIn
+    produto <- requireJsonBody :: Handler Compra -- fazer parse para objeto 
+    utct <- liftIO getCurrentTime
+    pid <- runDB $ insert $ produto {compraDataAbertura = utctDay utct, compraHoraAbertura = utct,compraHoraFecha = utct} -- 
+    prod <- runDB $ selectList [CompraId ==. pid ] [] 
+    sendStatusJSON created201 (toJSON prod )
+
+getFechamentoR :: Handler TypedContent
+getFechamentoR = do 
    anyOriginIn
    prods <- runDB $ selectList [] [Asc CompraDataFecha] 
    sendStatusJSON ok200 (toJSON prods)
 
-postCompraAddR :: Handler TypedContent
-postCompraAddR = do
+getFechamentoUmR :: CompraId -> Handler TypedContent
+getFechamentoUmR pid = do 
     anyOriginIn
-    produto <- requireJsonBody :: Handler Compra
+    prod <- runDB $ get404 pid
+    sendStatusJSON ok200 (toJSON prod)
+
+getFechamentoPorData :: Day -> Handler TypedContent
+getFechamentoPorData d = do 
+    anyOriginIn
+    prods <- runDB $ selectList [CompraDataAbertura ==. d ] [Asc CompraDataFecha] 
+    sendStatusJSON ok200 (toJSON prods)
+    
+getFechamentoPorHoraFechamentoR :: Text -> Day -> Handler TypedContent
+getFechamentoPorHoraFechamentoR hora dia = do 
+    anyOriginIn
+    prods <- runDB $ selectList [CompraDataFecha ==. dia ] [Asc CompraDataFecha] --CompraHoraFecha >=. hora ,
+    sendStatusJSON ok200 (toJSON prods)
+    
+
+patchFechamentodataUpdateR :: CompraId -> Day -> Handler TypedContent
+patchFechamentodataUpdateR pid nome = do 
+    _ <- runDB $ get404 pid
+    anyOriginIn
+    runDB $ update pid [CompraDataFecha =. nome]
+    sendStatusJSON noContent204 (object [])
+
+patchFechamentohoraUpdateR :: CompraId -> Text -> Handler TypedContent
+patchFechamentohoraUpdateR pid nome = do 
+    _ <- runDB $ get404 pid
+    anyOriginIn
     utct <- liftIO getCurrentTime
-    pid <- runDB $ insert $ produto {compraDataAbertura = utctDay utct, compraHoraAbertura = utct,compraHoraFecha = utct}
-    prod <- runDB $ selectList [CompraId ==. pid ] [] 
-    sendStatusJSON created201 (toJSON prod )
+    runDB $ update pid [CompraHoraFecha =. utct]
+    sendStatusJSON noContent204 (object [])
+
+patchFechamentovalorUpdateR :: CompraId -> Text -> Handler TypedContent
+patchFechamentovalorUpdateR pid nome = do 
+    _ <- runDB $ get404 pid
+    anyOriginIn
+    runDB $ update pid [CompraValor =. (  R.read ( unpack nome) ::Double) ] --readMaybe 
+    sendStatusJSON noContent204 (object [])
+    
+    
+    
+optionsFechamentodataUpdateR :: CompraId -> Day -> Handler TypedContent
+optionsFechamentodataUpdateR pid nome = do 
+    anyOriginIn
+    sendStatusJSON ok200 (object [])
+optionsFechamentohoraUpdateR :: CompraId -> Text -> Handler TypedContent
+optionsFechamentohoraUpdateR pid nome = do 
+    anyOriginIn
+    sendStatusJSON ok200 (object [])
+
+optionsFechamentovalorUpdateR :: CompraId -> Text -> Handler TypedContent
+optionsFechamentovalorUpdateR pid nome = do 
+    anyOriginIn
+    sendStatusJSON ok200 (object [])
+
+
+optionsFechamentoPorHoraFechamentoR :: Text -> Day -> Handler TypedContent
+optionsFechamentoPorHoraFechamentoR _ _  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+
+optionsFechamentoPorData :: Day -> Handler TypedContent
+optionsFechamentoPorData _  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+
+optionsFechamentoAddR :: Handler TypedContent
+optionsFechamentoAddR  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+
+optionsFechamentoR :: Handler TypedContent
+optionsFechamentoR  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+
+optionsFechamentoUmR :: CompraId -> Handler TypedContent
+optionsFechamentoUmR _  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
