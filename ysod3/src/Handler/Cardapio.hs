3cfd2b98fdfe372a385e70b73367a0ee029e3fb6
CoelhoGabrielGit <44949020+CoelhoGabrielGit@users.noreply.github.com>
2018-12-08 12:51:57 -0200

    Update Cardapio.hs
    Adicionado o patch e o resto dos options

--- ysod3/src/Handler/Cardapio.hs
+++ ysod3/src/Handler/Cardapio.hs
@@ -22,3 +22,24 @@ postCardapioAddR = do
     pid <- runDB $ insert produto
     sendStatusJSON created201 (toJSON pid)
 
+patchCardapioupdateR :: CardapioId -> Text -> Handler TypedContent
+patchCardapioupdateR pid nome = do 
+    _ <- runDB $ get404 pid
+    anyOriginIn
+    runDB $ update pid [CardapioDisponivel =. (  R.read ( unpack nome) :: Bool) ] --readMaybe 
+    sendStatusJSON noContent204 (object [])
+    
+optionsCardapioupdateR :: CardapioId -> Text -> Handler TypedContent
+optionsCardapioupdateR pid nome = do 
+    anyOriginIn
+    sendStatusJSON ok200 (object [])
+    
+optionsCardapioR :: Handler TypedContent
+optionsCardapioR  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+
+optionsCardapioAddR :: Handler TypedContent
+optionsCardapioAddR  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
