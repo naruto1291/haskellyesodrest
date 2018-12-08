5198f9db630f07101e3cc1350004b412c8307ffa
CoelhoGabrielGit <44949020+CoelhoGabrielGit@users.noreply.github.com>
2018-12-08 11:06:18 -0200

    Update CardapioPedido.hs
    Adicionados novos gets e seus options

--- ysod3/src/Handler/CardapioPedido.hs
+++ ysod3/src/Handler/CardapioPedido.hs
@@ -31,3 +31,29 @@ optionsCardapioPedidoR :: Handler TypedContent
 optionsCardapioPedidoR  = do
    anyOriginIn
    sendStatusJSON ok200 (object [])
+   
+getCardapioPedidoJoinR :: PedidoId   -> Handler TypedContent        
+getCardapioPedidoJoinR pid = do 
+    anyOriginIn
+    vendas <- runDB $ selectList [ CardapioPedidoPedido ==. pid] []
+    proids <- return $ fmap( cardapioPedidoCardapio . entityVal) vendas
+    clientes <- runDB $ selectList[CardapioId <-. proids ][]
+    sendStatusJSON ok200 (toJSON  clientes)   
+
+optionsCardapioPedidoJoinR :: PedidoId   -> Handler TypedContent
+optionsCardapioPedidoJoinR _  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
+   
+getCardapioPedidoDuploJoinR :: PedidoId   -> Handler TypedContent        
+getCardapioPedidoDuploJoinR pid = do 
+    anyOriginIn
+    vendas <- runDB $ selectList [ CardapioPedidoPedido ==. pid] []
+    proids <- return $ fmap( cardapioPedidoCardapio . entityVal) vendas
+    clientes <- runDB $ selectList[CardapioId <-. proids ][]
+    sendStatusJSON ok200  (object ["pedidoCardapio" .= vendas ,"cardapio" .= (clientes),"pedidoid".=pid  ])
+
+optionsCardapioPedidoDuploJoinR :: PedidoId   -> Handler TypedContent   
+optionsCardapioPedidoDuploJoinR _  = do
+   anyOriginIn
+   sendStatusJSON ok200 (object [])
